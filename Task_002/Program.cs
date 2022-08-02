c614ea07df244d61c75d6160b5e1051cafe36472
Andrii Korshenko <andriikorshenko@gmail.com>
2022-08-02 18:11:34 +0300

    Initial commit

---
+++ Task_002/Program.cs
@@ -0,0 +1,61 @@
+﻿var service = new Service(new AgregateLoggers(
+    new FileLogger(), 
+    new ConsoleLogger()
+    ));
+service.BusinessLogic();
+
+class Service
+{
+    ILogger _logger;
+
+    public Service(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void BusinessLogic()
+    {
+        _logger.LogTo("some log");
+    }
+}
+
+interface ILogger
+{
+    void LogTo(string log);
+}
+
+class ConsoleLogger : ILogger
+{
+    void ILogger.LogTo(string log)
+    {
+        Console.WriteLine(log);
+    }
+}
+
+class FileLogger : ILogger
+{
+    string path = @"loggfile.txt";
+
+    void ILogger.LogTo(string log)
+    {
+        File.AppendAllText(path, log + Environment.NewLine);
+    }
+}
+
+class AgregateLoggers : ILogger
+{
+    private readonly ILogger[] _loggers;
+
+    public AgregateLoggers(params ILogger[] loggers)
+    {
+        _loggers = loggers;
+    }
+
+    void ILogger.LogTo(string log)
+    {
+        foreach (var logger in _loggers)
+        {
+            logger.LogTo(log);
+        }
+    }
+}
---
+++ Task_006/Program.cs
@@ -0,0 +1,31 @@
+﻿var myFlag = new Derived();
+myFlag.DrawAFlag();
+
+abstract class Base
+{
+    public void DrawAFlag()
+    {
+        UpperColor();
+        BottomCollor();
+    }
+
+    protected abstract void UpperColor();
+    protected abstract void BottomCollor();
+}
+
+class Derived : Base
+{
+    protected override void UpperColor()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(new String('█', 7));
+        Console.ResetColor();
+    }
+
+    protected override void BottomCollor()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(new String('█', 7));
+        Console.ResetColor();
+    }
+}
